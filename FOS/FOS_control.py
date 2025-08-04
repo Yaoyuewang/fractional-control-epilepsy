3bc4a5d8286e3e1ae9b139254999d2696755094e
Yaoyue Wang <103149489+Yaoyuewang@users.noreply.github.com>
2025-08-04 11:18:20 -0700

    fix ctrl add plots

--- FOS/FOS_control.py
+++ FOS/FOS_control.py
@@ -4,23 +4,24 @@ import numpy as np
 import scipy.io
 import math 
 
-patients = ['HUP64', 'HUP68','HUP70','HUP72','HUP78','HUP86','MAYO010','MAYO011','MAYO016','MAYO020']
+# patients = ['HUP64', 'HUP68','HUP70','HUP72','HUP78','HUP86','MAYO010','MAYO011','MAYO016','MAYO020']
+patients = ['HUP64', 'HUP68','HUP72','HUP78','HUP86','MAYO010','MAYO011','MAYO016','MAYO020']
 # patients = ['HUP86']
 main_pathname = 'c:/Users/yaoyu/Documents/Epilepsy_research/'
 
 window_length = 3
-
+ictal_windows = 18
 for patient in patients:
     for seizure in range(1, 9):  
         filepath = os.path.join(main_pathname, 'data', patient, f"{patient}-ictal-block-{seizure}.mat")
         if os.path.exists(filepath):
             print(f"{patient}_{seizure}")
-            results = os.path.join(main_pathname, 'Results', patient, f"ictal-block-{seizure}")
-            FOS_params = scipy.io.loadmat(os.path.join(main_pathname, 'data', patient, f"ictal-block-{seizure}_ictal_parameters_3sec_1iter.mat"))
-            raw_data = scipy.io.loadmat(os.path.join(main_pathname, 'data', patient, f"ictal-block-{seizure}_ictal_parameters_3sec_1iter.mat"))
+            # results = os.path.join(main_pathname, 'Results', patient, f"ictal-block-{seizure}")
+            FOS_params = scipy.io.loadmat(os.path.join(main_pathname, 'data_v2', patient, f"ictal-block-{seizure}_parameters_3sec_1iter.mat"))
+            raw_data = scipy.io.loadmat(filepath)
             sampling_rate = int(np.ceil(raw_data['Fs'][0][0]))
-            A_0 = FOS_params["A_0"]
             A = FOS_params["A"]
+            A_0 = FOS_params["A_0"]
             alpha = FOS_params["alpha"]
             num_chns = FOS_params["A"].shape[0]
             evData = raw_data["evData"]
@@ -56,45 +57,47 @@ for patient in patients:
             opt_P = np.array(P.value)
 
             A_tilde = np.matmul(opt_L,np.linalg.inv(opt_P))
+            X = evData[:,ictal_start:]
+            num_chns = X.shape[0]
+            eigs_before = np.zeros((num_chns, ictal_windows), dtype=np.complex128)
+            eigs_after  = np.zeros_like(eigs_before)
+
             infit = 5  # the maximum number of states in the past should be considered          
             numStep = 1 # number of steps ahead
             p = 1 # number of past states
-            TSteps = min(sampling_rate * 30, (alpha.shape[1] - 18) * sampling_rate)
-
-            xPred = np.zeros((num_chns, TSteps * numStep))
-            XTemp = np.zeros_like(xPred)
-            warm = max(p, infit) * numStep          
-            xPred[:, :warm] = evData[:, :warm]
-            XTemp[:, :warm] = evData[:, :warm]
-            
-            for i in range(1, TSteps):
-                XTemp[:, i * numStep - 1] = evData[:, i * numStep - 1]
+            TSteps = sampling_rate*ictal_windows
+            #simulated data 
+            xPred = np.zeros((num_chns, TSteps*numStep))
+            xPred[:, 0:numStep] = X[:, 0:numStep]
+            XTemp = np.zeros((num_chns, TSteps*numStep))
+            for i in range(1,TSteps):
+                XTemp[:, 0:i*numStep] = X[:, 0:i*numStep]
                 for stepInd in range(numStep):
-                    t = i * numStep + stepInd          
                     for chInd in range(num_chns):
-                        alpha_inst = alpha[chInd, ictal_start + math.floor(i / sampling_rate)]
+                        alpha_inst = alpha[chInd,ictal_start+math.floor(i/sampling_rate)]
+                        #check whether alpha is fractional
                         if math.ceil(alpha_inst) != alpha_inst:
-                            trailLen = min(infit, t - 1)
-                            if trailLen > 0:
-                                j = np.arange(1, trailLen + 1)
-                                preFact = scipy.special.gamma(-alpha_inst + j) / (
-                                    scipy.special.gamma(-alpha_inst) *
-                                    scipy.special.gamma(j + 1)
-                                )
-                                XTemp[chInd, t] = (
-                                    XTemp[chInd, t]
-                                    - np.sum(XTemp[chInd, t - j] * preFact)
-                                )
-                    XUse = np.zeros((num_chns, p))
+                            trailLen = np.min(np.array([infit, i*numStep + stepInd - 1]))
+                            j = np.arange(1, trailLen + 1)
+                            preFact = scipy.special.gamma(-alpha_inst + j) / (scipy.special.gamma(-alpha_inst) * scipy.special.gamma(j + 1))
+                            XTemp[chInd, i* numStep + stepInd] = XTemp[chInd, i*numStep + stepInd] - np.sum(XTemp[chInd, i*numStep + stepInd - j] * preFact)
+
+                    XUse = np.zeros((num_chns,p))
                     for pInd in range(p):
-                        lag = t - pInd * numStep      
-                        if lag < 0:
+                        if i*numStep + stepInd - pInd < 1:
                             break
-                        XUse[:, pInd] = XTemp[:, lag]
-                    A_current = A[:, :, ictal_start + math.floor(i / sampling_rate)] + A_tilde
-                    contrib   = (A_current @ XUse).sum(axis=1)     
-                    XTemp[:, t] += contrib
-                xPred[:, i * numStep : (i + 1) * numStep] = XTemp[:, i * numStep : (i + 1) * numStep]
-            savepath = os.path.join(main_pathname, 'data', patient, f"controlled_data_block_{seizure}.mat")
-            scipy.io.savemat(savepath, {"evData": xPred, "Fs": sampling_rate})
+                        XUse[:, pInd] = XTemp[:, i*numStep + stepInd - pInd]
+                    A_no_ctrl = A_0[:, :, ictal_start + math.floor(i / sampling_rate)]
+                    A_current = A_no_ctrl + A_tilde
+                    XTemp[:, i*numStep + stepInd] = XTemp[:, i*numStep + stepInd] + np.dot(A[:,:,ictal_start+math.floor(i/sampling_rate)]+A_tilde, XUse[:, pInd]) 
+
+                xPred[:, (i)*numStep:(i+1)*numStep] = XTemp[:, (i)*numStep:(i+1)*numStep]
+ 
+                if (i - 1) % sampling_rate == 0:
+                    col = (i - 1) // sampling_rate
+                    eigs_before[:, col] = np.linalg.eigvals(A_no_ctrl)
+                    eigs_after [:, col] = np.linalg.eigvals(A_current)
+            
+            savepath = os.path.join(main_pathname, 'data_v2', patient, f"controlled_data_block_{seizure}.mat")
+            scipy.io.savemat(savepath, {"evData": xPred, "Fs": sampling_rate, "eigs_before": eigs_before, "eigs_after": eigs_after})
 
--- FOS_analysis/extract_features.py
+++ FOS_analysis/extract_features.py
@@ -6,9 +6,9 @@ patients = ['HUP64', 'HUP68','HUP70','HUP72','HUP78','HUP86','MAYO010','MAYO011'
 # patients = ['HUP64']
 main_pathname = 'c:/Users/yaoyu/Documents/Epilepsy_research/data_v2'
 window_size = 3
-ictal_start = 20 
 
-
+data_length = 20 - window_size + 1
+ictal_start = data_length
 def load_fos_params(patient, seizure, condition, window_size):
     path = f'{main_pathname}/{patient}/{condition}-block-{seizure}_parameters_{window_size}sec_1iter.mat'
     return scipy.io.loadmat(path)
@@ -20,21 +20,21 @@ for patient in patients:
             ictal_data = load_fos_params(patient, seizure, 'ictal', window_size)
             interictal_data = load_fos_params(patient, seizure, 'interictal', window_size)
             num_windows = ictal_data['alpha'].shape[1]
-            ictal_end = min(num_windows-20, ictal_start + 20)
+            ictal_end = min(num_windows-20, ictal_start + data_length)
             
             ictal_alpha_all = ictal_data['alpha']
             ictal_eigen_all = ictal_data['eigenvalues']
 
-            interictal_alpha = interictal_data['alpha'][:, 0:20]
-            interictal_eigen = interictal_data['eigenvalues'][:, 0:20]
+            interictal_alpha = interictal_data['alpha'][:, 0:data_length]
+            interictal_eigen = interictal_data['eigenvalues'][:, 0:data_length]
 
             preictal_alpha = ictal_alpha_all[:, 0:ictal_start]
             ictal_alpha = ictal_alpha_all[:, ictal_start:ictal_end]
-            postictal_alpha = ictal_alpha_all[:, num_windows - 20:num_windows]
+            postictal_alpha = ictal_alpha_all[:, num_windows - 20:num_windows -2]
 
             preictal_eigen = ictal_eigen_all[:, 0:ictal_start]
             ictal_eigen = ictal_eigen_all[:, ictal_start:ictal_end]
-            postictal_eigen = ictal_eigen_all[:, num_windows-20:num_windows]
+            postictal_eigen = ictal_eigen_all[:, num_windows-20:num_windows - 2]
 
             save_path = os.path.join(main_pathname, patient, f"fos_features_block_{seizure}.npz")
             np.savez(save_path,
@@ -46,7 +46,7 @@ for patient in patients:
                 ictal_eigen = ictal_eigen,
                 postictal_eigen = postictal_eigen,
                 interictal_eigen = interictal_eigen,
-                timepoints = list(range(num_windows-20,num_windows)))
+                timepoints = list(range(num_windows-20,num_windows - 2)))
             
             
 
